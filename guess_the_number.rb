fc5210eabc2c5d22afe40f7363af689b532c6949
Carlitos09 <tighty63@hotmail.com>
2014-03-18 22:53:25 +0000

    created a guessing game

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,1 +1,10 @@
-puts "HELLO WORLD"
+puts "Welcome to the guessing game"
+secret_number = rand(6)
+puts "Take guess of what number is gonna be put out"
+guess = gets.chomp.to_i
+
+if secret_number == guess
+  puts "Congratulations"
+else
+  puts "Try again. The secret number was #{secret_number}"
+end
